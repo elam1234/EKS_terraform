d31b9ef4b9c9a8d10f566f854902613e27001a7d
elam1234 <152494447+elam1234@users.noreply.github.com>
2024-04-26 17:46:08 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "cidr_block" {
+    description = "this use to define a vpc ip range "
+  
+}
+
+variable "aws_subnet1" {
+    description = "this will define a subnet1 cidr range"
+  }
+
+variable "aws_subnet2" {
+    description = "this will be define a subnet2 cidr range"
+}
